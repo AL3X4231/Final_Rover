fd4578a781918d9f0372483eef6f1def35874e9e
Jérémy Lebourgeois <134381505+JeremyLebourgeois@users.noreply.github.com>
2024-11-21 11:41:29 +0100

    installation des costs dans l'arbre

--- node.h
+++ node.h
@@ -11,16 +11,16 @@
 typedef struct t_node {
     int value ;
     int nb_sons;
-    t_soil soil;
+    t_localisation loc;
     t_move move;
     struct t_node * sons[9];
 
 }t_node;
 
 
-t_node * createNode(int value,int nb_sons, t_move move);
+t_node * createNode(int value,int nb_sons, t_move move, t_localisation loc);
 
-void createSons(t_node* node, int height, int nb_choices,t_move moves[]);
+void createSons(t_node* node, int height, int nb_choices,t_move moves[],t_localisation loc, t_map map);
 void displayTree(t_node* node, int level);
 
 
