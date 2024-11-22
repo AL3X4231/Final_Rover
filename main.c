e2dbe5f32219b2186a25278790b5d58a3541f492
AL3X4231 <alex2005morin@gmail.com>
2024-11-22 17:40:32 +0100

    Update about near coordinate

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "map.h"
 #include "node.h"
 #include "moves.h"
 #include "loc.h"
+#define INT_MAX 2147483647
+
+t_position findNearestReachablePoint(t_map map) {
+    t_position basePos = getBaseStationPosition(map);
+    t_position nearestPoint = { .x = -1, .y = -1 };
+    int minDistance = INT_MAX;
+
+    for (int i = 0; i < map.y_max; i++) {
+        for (int j = 0; j < map.x_max; j++) {
+            // Skip crevasses and base station
+            if (map.soils[i][j] == CREVASSE || map.soils[i][j] == BASE_STATION) {
+                continue;
+            }
+
+            // Calculate Manhattan distance to base
+            int distance = abs(j - basePos.x) + abs(i - basePos.y);
+            
+            // Update nearest point if this point is closer
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestPoint.x = j;
+                nearestPoint.y = i;
+            }
+        }
+    }
+
+    if (nearestPoint.x != -1 && nearestPoint.y != -1) {
+        printf("Nearest reachable point to base is at: [%d, %d] with distance: %d\n", 
+               nearestPoint.x, nearestPoint.y, minDistance);
+    } else {
+        printf("No reachable point found.\n");
+    }
+
+    return nearestPoint;
+}
 
 int main() {
     t_map map;
@@ -16,21 +52,21 @@ int main() {
     map = createMapFromFile("../maps/example1.map");
 #endif
 
-    /*printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
+    printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
     for (int i = 0; i < map.y_max; i++){
         for (int j = 0; j < map.x_max; j++){
             printf("%d ", map.soils[i][j]);
         }
         printf("\n");
-    }*/
+    }
     // printf the costs, aligned left 5 digits
-    /*for (int i = 0; i < map.y_max; i++){
+    for (int i = 0; i < map.y_max; i++){
         for (int j = 0; j < map.x_max; j++){
             printf("%-5d ", map.costs[i][j]);
         }
         printf("\n");
     }
-    displayMap(map);*/
+    displayMap(map);
 
 
     //localisation de base
@@ -39,7 +75,7 @@ int main() {
 
     // tests arbre
     int nb_moves = 9;  //test avec 4 moves differents
-    int nb_choices = 9 ;  // on en prend que 3 sur les 4
+    int nb_choices = 5  ;  // on en prend que 3 sur les 4
     t_move* moves = chooseMove(nb_moves);
     t_node* root1 = createNode(0, nb_moves, STAY, startLoc ); //root de l'arbre avec move = "STAY"
     createSons(root1, nb_moves, nb_choices, moves, startLoc, map);
@@ -56,12 +92,18 @@ int main() {
 
     t_path best_path = {0};
     int min_cost = evaluateTree(root1, &best_path);
-    printf("\nOptimal path to base (total cost: %d):\n", min_cost);
-    for (int i = 0; i < best_path.num_moves; i++) {
-        t_localisation newLoc = move(startLoc, best_path.moves[i]);
-        int move_cost = map.costs[newLoc.pos.y][newLoc.pos.x];
-        printf("%d. %s (cost: %d)\n", i+1, t_move_to_string(best_path.moves[i]), move_cost);
-        startLoc = newLoc;  // Update position for next move
+
+    if (min_cost == INT_MAX) {
+        printf("No valid path to base found.\n");
+        findNearestReachablePoint(map);
+    } else {
+        printf("\nOptimal path to base (total cost: %d):\n", min_cost);
+        for (int i = 0; i < best_path.num_moves; i++) {
+            t_localisation newLoc = move(startLoc, best_path.moves[i]);
+            int move_cost = map.costs[newLoc.pos.y][newLoc.pos.x];
+            printf("%d. %s (cost: %d)\n", i+1, t_move_to_string(best_path.moves[i]), move_cost);
+            startLoc = newLoc;  // Update position for next move
+        }
     }
 
     return 0;
